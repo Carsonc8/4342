d8528862337c546242ea91ef35981a2651605390
Carsonc8 <carsonc898@gmail.com>
2023-09-25 18:21:00 -0500

    Added vars

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -1,2 +1,22 @@
 import re
 import sys
+"""
+    Types of Tokens:
+    Special
+    Identifier
+    Data Type
+    Reserved
+    Assignment
+    Multiplication
+    Relation
+    Reserved
+    Addition
+    Integer
+"""
+
+RESERVED_TOKENS = {
+
+}
+IDENTIFIER_TOKEN = ''
+INTEGER_TOKENS = ''
+SPECIAL_TOKENS = ''
