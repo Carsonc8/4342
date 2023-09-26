8e8322fd58aa5340f16b4c4136468959329a1d9e
Carsonc8 <carsonc898@gmail.com>
2023-09-26 17:42:19 -0500

    rewrote and created new lists

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -14,16 +14,36 @@ import sys
     Integer
 """
 
-RESERVED_TOKENS = {}
-# r converts it to a raw string. Then reads as any letter from a-zA-Z along with an _
-IDENTIFIER_REGEX = [r'[a-zA-Z_d]\w*']
-INTEGER_TOKENS = ''
-SPECIAL_TOKENS = ''
 
+# r converts it to a raw string. Then reads as any letter from a-zA-Z along with an _ that allows any combination of letters.
+IDENTIFIER_REGEX = [r'[a-zA-Z_d]\w*']
+#any numbers with any combination
+INTEGER_TOKENS = ['\d+']
+ADDITION_TOKEN = []
+MULTIPLYING_TOKEN = []
+SPECIAL_KEYWORD = []
+ASSIGNMENT_OPERATOR = []
+RELATIONAL_TOKEN = []
+DATA_TYPE_TOKEN = []
+
+"""
 for patterns in IDENTIFIER_REGEX:
-    wow = re.findall(patterns, "f")
-print(wow)
-if wow:
-    print("Valid Identifier")
+    alpha = re.findall(patterns, "f")
+
+if alpha:
+    print("Valid Letter Identifier")
 else:
     print("Not Valid Identifier")
+for numPatterns in INTEGER_TOKENS:
+    num = re.findall(numPatterns, "30")
+if num:
+    print("Valid Number Identifier")
+else:
+    print("Not Valid Identifier")
+for specialPat in SPECIAL_TOKENS:
+    special = re.findall(specialPat, ";")
+if special:
+    print("Valid Special Identifier")
+else:
+    print("Not Valid Identifier")
+"""
