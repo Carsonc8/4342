39ff036e08fd79f3de81ec36ae9e06614b673fa8
Carsonc8 <carsonc898@gmail.com>
2023-09-26 16:25:05 -0500

    Added a validation for regex pattern

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -14,9 +14,16 @@ import sys
     Integer
 """
 
-RESERVED_TOKENS = {
-
-}
-IDENTIFIER_TOKEN = ''
+RESERVED_TOKENS = {}
+# r converts it to a raw string. Then reads as any letter from a-zA-Z along with an _
+IDENTIFIER_REGEX = [r'[a-zA-Z_d]\w*']
 INTEGER_TOKENS = ''
 SPECIAL_TOKENS = ''
+
+for patterns in IDENTIFIER_REGEX:
+    wow = re.findall(patterns, "f")
+print(wow)
+if wow:
+    print("Valid Identifier")
+else:
+    print("Not Valid Identifier")
